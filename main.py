f36fdfe228300aa996b5335d03e0a0674a3de1cc
Ahmad Alobaid <ahmad88me@gmail.com>
2018-05-17 14:30:57 +0200

    working on generating the list

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ import os.path
 from google.cloud import translate
 from flask import Flask, render_template, send_from_directory
 from jinja2 import Template
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
+@app.route('/data/<path:path>')
+def send_data(path):
+    print("data is requested:\n\n\n")
+    return send_from_directory('data', path)
 
 @app.route('/js/<path:path>')
 def send_js(path):
@@ -18,69 +25,42 @@ def send_js(path):
 
 @app.route("/")
 def hello():
-    # jsonFileInString = loadJSON("https://www.zaragoza.es/sede/servicio/equipamiento/category/740.json")
     jsonfile = "740.json"
-
     j_text = load_json("740.json")
     equipamientos = j_text["equipamiento"]
-
-    #print("equipamientos = %s"%equipamientos)
-
     translate_client = translate.Client()
     cache_filename = "cache.txt"
-
     if os.path.isfile(cache_filename):
         cache = load_json(cache_filename)
     else:
         cache = {}
-
     for equipamiento in equipamientos:
-        #print("equipamiento = %s"%equipamiento)
-        #print("equipamiento.keys() = %s"%equipamiento.keys())
-
         if "horario" in equipamiento.keys():
             original_text = equipamiento["horario"]
-            #print("original_text = %s"%original_text)
             target = 'en'
-
             if original_text in cache.keys():
-                #print("using cache")
-
                 translation = cache[original_text]
-
             else:
                 # Translates some text into English
                 translation = translate_client.translate(
                     equipamiento["horario"],
                     target_language=target)
                 print("using google service")
-
                 cache[original_text] = translation
-
             equipamiento["opening_hours"] = translation["translatedText"]
-
-    #return "equipamientos = %s"%equipamientos
-
-
-
-
-
     cache_string = json.dumps(cache)
     new_file = open("cache.txt", 'w')
     new_file.write(cache_string)
-
-
     return render_template('template.html', items=equipamientos)
 
 def load_json(filename):
     f = open(filename)
     f_text = f.read()
     j_text = json.loads(f_text)
-    #print("j_text = %s"%j_text)
-
     return j_text
 
 
 if __name__ == '__main__':
+    CORS(app)
     app.run(debug=True)
 
